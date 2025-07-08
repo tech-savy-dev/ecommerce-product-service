e3074be0c6efcd0eefb5e2c50304e57405160496
tech-savy-dev <techsavydev@gmail.com>
2025-07-08 12:38:48 +0800

    commit for deploy chnages

--- src/main/java/com/ecommerce/controllers/ProductController.java
+++ src/main/java/com/ecommerce/controllers/ProductController.java
@@ -11,7 +11,7 @@ public class ProductController {
     @GetMapping("/productCheck")
     public String productHealthCheck(){
 
-        System.out.println("hello msg");
+        System.out.println("hello msg23");
         return "Product Service Working properly";
     }
 }
