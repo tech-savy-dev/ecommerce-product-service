e3c05e6d89b43cf082a9d73158a150073de15936
tech-savy-dev <techsavydev@gmail.com>
2025-07-29 12:58:48 +0000

    gitpod

--- src/main/java/com/ecommerce/controllers/ProductController.java
+++ src/main/java/com/ecommerce/controllers/ProductController.java
@@ -11,7 +11,7 @@ public class ProductController {
     @GetMapping("/productCheck")
     public String productHealthCheck(){
 
-        System.out.println("hello msg45 gitpod");
+        System.out.println("hello msg45 Gitcopilot");
         return "Product Service Working properly";
     }
 }
