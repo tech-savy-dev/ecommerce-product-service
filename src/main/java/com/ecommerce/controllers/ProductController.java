ad5bcbbe1f1ec53b1bbeb4fd78e52dbefb7386b1
harish9542 <harishbabu0419@gmail.com>
2025-07-29 13:55:39 +0000

    COmmit

--- src/main/java/com/ecommerce/controllers/ProductController.java
+++ src/main/java/com/ecommerce/controllers/ProductController.java
@@ -11,7 +11,7 @@ public class ProductController {
     @GetMapping("/productCheck")
     public String productHealthCheck(){
 
-        System.out.println("hello msg45 Harishbabu");
+        System.out.println("hello msg45 Marella");
         return "Product Service Working properly";
     }
 }
